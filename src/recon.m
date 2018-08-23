fdf629574e42a3dbf33fae7146fd2cca341a8858
Emily <eclark7@uw.edu>
2018-08-23 14:12:30 -0700

    Streamlined and added documentation

--- src/recon.m
+++ src/recon.m
@@ -1,19 +1,40 @@
-function Error = recon(Psi,Y,ind,N)
+function Error = recon(Psi,Y,ind)
+% Calculate the reconstruction error for the test set Y given sensors ind
+% and basis functions Psi.
+%
+% Input:
+%
+%   Psi - the n by r matrix of basis functions
+%   Y - the n by m matrix of test set snapshots
+%   ind - the 1 by p vector of sensor indices
+%
+% Output:
+%
+%   Error - the average reconstruction error for the test set
+%
+% Example:
+%
+%   >> Error = recon(Psi,Y,ind);
+%
+% Copyright Emily Clark 2017
+% Available freely under the MIT License
+
+[n,m] = size(Y);
 
 theta = Psi(ind,:);
 theta_inv = pinv(theta);
 
-for jj = 1:size(Y,2)
-    ytrue = Y(:,jj);
+E = zeros(1,m);
+for j = 1:m
+    ytrue = Y(:,j);
     ysparse = ytrue(ind);
     
-    % Reconstruct the image
+    % Reconstruct the snapshot
     s = theta_inv*ysparse;
     yrecon = Psi*s;
     
     % Calculate the error
-%     E(jj) = norm(abs(yrecon - ytrue))/sqrt(N);
-    E(jj) = norm(abs(yrecon - ytrue))/norm(ytrue);
+    E(j) = norm(abs(yrecon - ytrue))/norm(ytrue);
 end
 
 Error = mean(E);
