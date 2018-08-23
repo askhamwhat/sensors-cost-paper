bcc91ad1489577a163a621f06f7053bbd2eda8dd
Emily <eclark7@uw.edu>
2018-08-23 15:38:45 -0700

    Finalized the structure. Still need to make the plots.

--- src/ReconstructionFaces.m
+++ src/ReconstructionFaces.m
@@ -7,37 +7,36 @@ Z = load('C:\Users\Emily\Documents\Research\FinalCodes\YaleB_32x32.mat');
 Z = Z.fea';
 
 [N,L] = size(Z);
-n = 32; % Images are 32 by 32 pixels
+n = 32;
 r = round(L*.80); % The size of the test set
 
-% Construct the cost function
-f2 = zeros(n,n);
-
-% Gaussian
+% Construct the cost function (uncomment your choice)
+% %Gaussian
 [X1,X2] = meshgrid(-n/2:n/2-1,-n/2:n/2-1);
 f2 = exp(-0.05*X1.^2 - 0.05*X2.^2);
 f = reshape(f2,N,1);
 Gamma = 10^4*[0:0.1:1,1.25,1.5:0.5:4,5:2:19]; % Cost function weightings
 
-% Block off the center
+% %Center blocked
+% f2 = zeros(n,n);
 % f2(11:22,11:22) = 1;
 % f = reshape(f2,N,1);
 % Gamma = 10^4*[0:0.05:0.5,0.6:0.1:1,1.5:0.5:6];
 
-% Block off left third
+% %Left blocked
+% f2 = zeros(n,n);
 % f2(:,1:11) = 1;
 % f = reshape(f2,N,1);
 % Gamma = 10^4*[0:0.05:0.5,0.6:0.1:1,1.5:0.5:6];
 
 
-% p = [5,10,15,25,50,75,100:50:400]; % The number of sensors
-p = 200;
-LCV = 2; % The number of cross validations
+p = [5,10,15,25,50,75,100:50:400]; % The number of sensors
+LCV = 20; % The number of cross validations
 
 % Obtain sensor locations, reconstruction errors, and costs
 [Cost,Error,A,stdE,stdC] = CostError(Z,f,Gamma,p,r,LCV);
 
-plot(Cost,Error,'k.')
+plot(Error,Cost,'k.')
 
 figure
 subplot(1,2,1)
